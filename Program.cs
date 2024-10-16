41724d88708aac8b8349beec4bba7321427e9af1
zxXf=5^;4.PuDhrt'~n9_U <a31864@alunos.ipca.pt>
2024-10-16 08:11:02 +0100

    create Aluno dll

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-using System.Threading.Tasks.Dataflow;
+using MyLibrary;
 
-class Program
-{
+class Program{
     static void Main(string[] args)
     {
+        Aluno aluno = new Aluno();
+        aluno.SetName("sandro");
+
         Console.WriteLine("hello world!");
+        Console.Write("I am the student: ");
+        if(aluno.GetOld() == null) aluno.SeOld("28");
+        Console.WriteLine(String.Format("{0} i am {1} years old", aluno.GetName(), aluno.GetOld()));
+
     }
 
 }
--- obj/Debug/net8.0/hello.AssemblyInfo.cs
+++ obj/Debug/net8.0/hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de13785fb13d7473c08330ef1ecf88d480de156b")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
